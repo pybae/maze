4950be3eb0ba70ef5d266e4eb0b0f1f17055e18f
Zane Urbanski <zane.urbanski@gmail.com>
2015-04-27 15:49:06 -0500

    pretty much completed outline for MazeGenerator

--- src/maze/MazeGenerator.java
+++ src/maze/MazeGenerator.java
@@ -1,7 +1,62 @@
+import java.util.ArrayList;
+import java.awt.Rectangle;
+
 public class MazeGenerator {
-    // Given a width, height, and windiness of the passages, this method will
-    //  return a MazeLayout object corresponding to the generated maze
-    public static MazeLayout generate(int w, int h, int windiness) {
-        return new MazeLayout(w, h);
+    public enum Direction { UP, DOWN, LEFT, RIGHT }
+
+    private int width;
+    private int height;
+    private int windiness;
+    private int minRoomSize;
+    private int maxRoomSize;
+    private int roomTries;
+    private int minRooms;
+
+    private ArrayList<Rectangle> rooms;
+    private int[][] regions;
+    private int currentRegion;
+
+    public MazeGenerator(int widthIn, int heightIn, int windinessIn,
+                         int minRoomSizeIn, int maxRoomSizeIn,
+                         int roomTriesIn, int minRoomsIn) {
+        width = widthIn;
+        height = heightIn;
+        windiness = windinessIn;
+        minRoomSize = minRoomSizeIn;
+        maxRoomSize = maxRoomSizeIn;
+        roomTries = roomTriesIn;
+        minRooms = minRoomsIn;
+
+        currentRegion = -1;
     }
+
+    // Using the width, height, and windiness of the passages, this method will
+    //  return a MazeLayout object corresponding to the generated maze
+    public MazeLayout generate() {
+        rooms = new ArrayList<Rectangle>();
+        regions = new int[height][width];
+
+        return new MazeLayout(width, height);
+    }
+
+    // Place rooms in the maze
+    private void addRooms(MazeLayout m) {}
+    
+    // Implements "growing tree" algorithm to build the maze
+    private void growMaze(MazeLayout m, int r, int c) {}
+
+    // Ensure that all regions are connected
+    private void connectRegions(MazeLayout m) {}
+
+    // Adds a junction at a specified location
+    private void addJunction(int r, int c) {}
+
+    // Determines whether an opening can be carved from the cell at the
+    //  specified location to the adjacent cell facing the specified direction
+    private boolean canCarve(int r, int c, Direction d) {
+        return false;
+    }
+
+    // Set the state at the specified location
+    private void carve(int r, int c, MazeLayout.State s) {}
 }
--- src/maze/MazeLayout.java
+++ src/maze/MazeLayout.java
@@ -1,10 +1,25 @@
 import java.util.*;
 
 public class MazeLayout {
-    public static enum state { WALL, PATH }
-    private state[][] maze;
+    public enum State { NOT_SET, WALL, PATH }
+
+    private State[][] maze;
 
     public MazeLayout(int w, int h) {
-        maze = new state[h][w];
+        maze = new State[h][w];
+
+        for(int i = 0; i < h; i++) {
+            for(int j = 0; j < w; j++) {
+                maze[i][j] = State.NOT_SET;
+            }
+        }
+    }
+
+    public State getState(int r, int c) {
+        return maze[r][c];
+    }
+
+    public void setState(int r, int c, State s) {
+        maze[r][c] = s;
     }
 }
