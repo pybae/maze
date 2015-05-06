1fcf7284a870fa1b09adf0e2312e56718603cc73
Paul Bae <pbae@utexas.edu>
2015-05-06 00:27:03 -0500

    refactored Maze and Player

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -3,8 +3,6 @@ package maze;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.shapes.SphereCollisionShape;
-import com.jme3.bullet.objects.PhysicsCharacter;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
 import com.jme3.font.BitmapText;
@@ -13,39 +11,20 @@ import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
-import com.jme3.light.SpotLight;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
 import com.jme3.math.Ray;
 import com.jme3.math.Vector3f;
-import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Node;
-import com.jme3.shadow.SpotLightShadowFilter;
-import com.jme3.shadow.SpotLightShadowRenderer;
 
 public class Maze extends SimpleApplication implements ActionListener {
 
     private BulletAppState bulletAppState;
     private Node doorNode, wallNode, openNode;
-    private PhysicsCharacter player;
-    private SpotLight flashLight1, flashLight2, flashLightRim;
-    private float lightInt1 = 0.95f;
-    private float lightInt2 = 0.315f;
-    private float lightIntRim = 0.30f;
-    private boolean left, right, up, down, sprint, optSprint;
-    private float headBob = 0;
-    private float headSway = 0;
-    private float lightSway = 0;
-    private Vector3f walkDirection = new Vector3f(0, 0, 0);
-    private static final float PLAYER_SPEED = 5.0f;
-    private static final float PLAYER_SPRINT = 3.0f;
+    private Player player;
     private static final float RENDER_DISTANCE = 50.0f;
-    private static final int SHADOWMAP_SIZE = 1024;
     private MazeGenerator generator;
     private MazeLayout layout;
-    private int spawnX;
-    private int spawnY;
+
     public Maze() {
     }
 
@@ -55,38 +34,29 @@ public class Maze extends SimpleApplication implements ActionListener {
          * The maze itself should be a 2D array of MazeEntities
          * we can initialize which coordinates to pass into the entities
          */
-         //test Room
-       MazeEntity mz = new WallEntity(16, 16);
-       OpenEntity oz = new OpenEntity(16, 16);
-       DoorEntity dz = new DoorEntity(16, 16);
-//        dz.renderObject(new Vector3f(0, 0, 16),
-//                        doorNode,
-//                        assetManager,
-//                        getPhysicsSpace());
-        //test Room
+        MazeEntity mz = new WallEntity(16, 16);
+        OpenEntity oz = new OpenEntity(16, 16);
+        DoorEntity dz = new DoorEntity(16, 16);
 
         generator = new MazeGenerator(61, 41, 100, 2, 6, 20, 1);
         layout = generator.generate();
         layout.print();
 
 
-        for(int r = 0; r < layout.maze.length; r++) {
-            for(int c = 0; c < layout.maze[0].length; c++) {
-                if(layout.maze[r][c] == State.WALL) {
+        for (int r = 0; r < layout.maze.length; r++) {
+            for (int c = 0; c < layout.maze[0].length; c++) {
+                if (layout.maze[r][c] == State.WALL) {
                     mz.renderObject(new Vector3f(16*r, 0, 16*c),
-                        wallNode,
-                        assetManager,
-                        getPhysicsSpace());
-                } else if(layout.maze[r][c] == State.DOOR) {
-                } else if(layout.maze[r][c] == State.NOT_SET) {
+                                    wallNode,
+                                    assetManager,
+                                    getPhysicsSpace());
+                } else if (layout.maze[r][c] == State.DOOR) {
+                } else if (layout.maze[r][c] == State.NOT_SET) {
                 } else {
-                        spawnX = r;
-                        spawnY = c;
-
-                        oz.renderObject(new Vector3f(16*r, 0, 16*c),
-                        openNode,
-                        assetManager,
-                        getPhysicsSpace());
+                    oz.renderObject(new Vector3f(16*r, 0, 16*c),
+                                    openNode,
+                                    assetManager,
+                                    getPhysicsSpace());
                 }
             }
             System.out.println();
@@ -112,166 +82,26 @@ public class Maze extends SimpleApplication implements ActionListener {
         rootNode.attachChild(wallNode);
         rootNode.attachChild(openNode);
 
-        generateMaze();
         initPlayer();
         initKeys();
-        initLight();
         initCrossHair();
+
+        generateMaze();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        Vector3f camDir = cam.getDirection();
-        Vector3f camLeft = cam.getLeft();
-        Vector3f camLoc = cam.getLocation();
-        float speed = PLAYER_SPEED;
-
-        Vector3f plLocation = player.getPhysicsLocation();
-        plLocation.setY(plLocation.getY() + 7f);
-
-        Vector3f lightDir = camDir;
-
-        walkDirection.set(0, 0, 0);
-
-        if (sprint) {
-            if(headBob < 360){
-                headBob += 10*tpf;
-                plLocation.setY(plLocation.getY() + 0.2f*FastMath.sin(headBob));
-                plLocation.setX(plLocation.getX() + 0.1f*FastMath.sin(headBob));
-            }
-            else{
-                headBob = 0;
-            }
-            if(lightSway < 360){
-                lightSway += 7*tpf;
-                lightDir.setY(lightDir.getY() + 0.025f*FastMath.sin(lightSway));
-                //lightDir.setX(lightDir.getX() + 0.05f*FastMath.sin(lightSway));
-                flashLight1.setColor(ColorRGBA.White.mult(lightInt1 + 0.2f*FastMath.sin(lightSway)));
-                flashLight2.setColor(ColorRGBA.Orange.mult(lightInt2 + 0.1f*FastMath.sin(lightSway)));
-                flashLightRim.setColor(ColorRGBA.Gray.mult(lightIntRim + 0.2f*FastMath.sin(lightSway)));
-            }
-            else{
-                lightSway = 0;
-            }
-            speed = PLAYER_SPRINT;
-        }
-        else {
-            if(headBob != 0){
-                headBob = 0;
-            }
-            if(lightSway != 0){
-                lightSway = 0;
-            }
-            flashLight1.setColor(ColorRGBA.White.mult(lightInt1));
-            flashLight2.setColor(ColorRGBA.Orange.mult(lightInt2));
-            flashLightRim.setColor(ColorRGBA.Gray.mult(lightIntRim));
-        }
-        if (left) {
-            walkDirection.addLocal(camLeft);
-        }
-        if (right) {
-            walkDirection.addLocal(camLeft.negate());
-        }
-        if (up) {
-            walkDirection.addLocal(camDir);
-        }
-        if (down) {
-            walkDirection.addLocal(camDir.negate());
-        }
-
-        walkDirection.setY(0);
-
-
-        player.setWalkDirection(walkDirection.divide(speed));
-        flashLight1.setPosition(camLoc);
-        flashLight2.setPosition(camLoc);
-        flashLightRim.setPosition(camLoc);
-
-        flashLight1.setDirection(lightDir);
-        flashLight2.setDirection(lightDir);
-        flashLightRim.setDirection(lightDir);
-
-        cam.setLocation(plLocation);
+        player.update(tpf);
     }
 
     private void initPlayer() {
         flyCam.setEnabled(true);
-
         cam.setFrustumFar(RENDER_DISTANCE);
 
-        player = new PhysicsCharacter(new SphereCollisionShape(3.0f), 0.1f);
+        player = new Player(rootNode, assetManager, cam, getPhysicsSpace(), viewPort);
 
-        // disallow player jump
-        player.setJumpSpeed(0);
-        player.setFallSpeed(20);
-        player.setGravity(30);
-        player.setPhysicsLocation(new Vector3f(spawnX*16, 5, spawnY*16));
-        getPhysicsSpace().add(player);
-    }
-
-    private void initLight() {
-        flashLight1 = new SpotLight();
-        flashLight1.setSpotRange(32);
-        flashLight1.setSpotInnerAngle(0f * FastMath.DEG_TO_RAD);
-        flashLight1.setSpotOuterAngle(36f * FastMath.DEG_TO_RAD);
-        flashLight1.setColor(ColorRGBA.White.mult(lightInt1));
-        flashLight1.setPosition(cam.getLocation());
-        flashLight1.setDirection(cam.getDirection());
-        rootNode.addLight(flashLight1);
-
-        flashLight2 = new SpotLight();
-        flashLight2.setSpotRange(38);
-        flashLight2.setSpotInnerAngle(32f * FastMath.DEG_TO_RAD);
-        flashLight2.setSpotOuterAngle(36f * FastMath.DEG_TO_RAD);
-        flashLight2.setColor(ColorRGBA.Orange.mult(lightInt2));
-        flashLight2.setPosition(cam.getLocation());
-        flashLight2.setDirection(cam.getDirection());
-        rootNode.addLight(flashLight2);
-
-        flashLightRim = new SpotLight();
-        flashLightRim.setSpotRange(28);
-        flashLightRim.setSpotInnerAngle(33f * FastMath.DEG_TO_RAD);
-        flashLightRim.setSpotOuterAngle(36f * FastMath.DEG_TO_RAD);
-        flashLightRim.setColor(ColorRGBA.Gray.mult(lightIntRim));
-        flashLightRim.setPosition(cam.getLocation());
-        flashLightRim.setDirection(cam.getDirection());
-        rootNode.addLight(flashLightRim);
-
-        SpotLightShadowRenderer slsr1 = new SpotLightShadowRenderer(assetManager,
-            SHADOWMAP_SIZE);
-        slsr1.setLight(flashLight1);
-        viewPort.addProcessor(slsr1);
-        SpotLightShadowFilter slsf1 = new SpotLightShadowFilter(assetManager,
-            SHADOWMAP_SIZE);
-        slsf1.setLight(flashLight1);
-        slsf1.setEnabled(true);
-        FilterPostProcessor fpp1 = new FilterPostProcessor(assetManager);
-        fpp1.addFilter(slsf1);
-        viewPort.addProcessor(fpp1);
-
-        SpotLightShadowRenderer slsr2 = new SpotLightShadowRenderer(assetManager,
-            SHADOWMAP_SIZE);
-        slsr2.setLight(flashLight2);
-        viewPort.addProcessor(slsr2);
-        SpotLightShadowFilter slsf2 = new SpotLightShadowFilter(assetManager,
-            SHADOWMAP_SIZE);
-        slsf2.setLight(flashLight2);
-        slsf2.setEnabled(true);
-        FilterPostProcessor fpp2 = new FilterPostProcessor(assetManager);
-        fpp2.addFilter(slsf2);
-        viewPort.addProcessor(fpp2);
-
-        SpotLightShadowRenderer slsrRim = new SpotLightShadowRenderer(assetManager,
-            SHADOWMAP_SIZE);
-        slsrRim.setLight(flashLightRim);
-        viewPort.addProcessor(slsrRim);
-        SpotLightShadowFilter slsfRim = new SpotLightShadowFilter(assetManager,
-            SHADOWMAP_SIZE);
-        slsfRim.setLight(flashLightRim);
-        slsfRim.setEnabled(true);
-        FilterPostProcessor fppRim = new FilterPostProcessor(assetManager);
-        fppRim.addFilter(slsfRim);
-        viewPort.addProcessor(fppRim);
+        // for now, set the player spawn to the center of the maze
+        player.setSpawn(30, 20);
     }
 
     private void initCrossHair(){
@@ -303,62 +133,31 @@ public class Maze extends SimpleApplication implements ActionListener {
     }
 
     public void onAction(String binding, boolean isPressed, float tpf) {
-        if (binding.equals("Left") && !isPressed) {
-            left = false;
-        } else if (binding.equals("Left")) {
-            left = isPressed;
-        } else if (binding.equals("Right") && !isPressed) {
-            right = false;
-        } else if (binding.equals("Right")) {
-            right = isPressed;
-        } else if (binding.equals("Up") && !isPressed) {
-            up = false;
-            sprint = false;
-        } else if (binding.equals("Up")) {
-            up = isPressed;
-        } else if (binding.equals("Down") && !isPressed) {
-            down = false;
-        } else if (binding.equals("Down")) {
-            down = isPressed;
-        } else if (binding.equals("Sprint") && !isPressed) {
-            sprint = false;
-            optSprint = false;
-        } else if (binding.equals("Sprint")) {
-            optSprint = isPressed;
-        }
-        else if (binding.equals("PointerAction") && !isPressed) {
-            //list of collisions from raycasting is stored in here
-            CollisionResults results = new CollisionResults();
-            Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+        if (!player.onAction(binding, isPressed, tpf)) {
+            if (binding.equals("PointerAction") && !isPressed) {
+                // list of collisions from raycasting is stored in here
+                CollisionResults results = new CollisionResults();
+                Ray ray = new Ray(cam.getLocation(), cam.getDirection());
 
-            //collisions being stored in results
-            doorNode.collideWith(ray, results);
-            //wallNode.collideWith(ray, results);
-            //openNode.collideWith(ray, results);
+                // collisions are stored in results
+                doorNode.collideWith(ray, results);
 
-            if(results.size() > 0){
-                CollisionResult closest = results.getClosestCollision();
+                if (results.size() > 0) {
+                    CollisionResult closest = results.getClosestCollision();
 
-                String hit = closest.getGeometry().getName();
-                System.out.println("Pointer picked " + hit + ".");
+                    String hit = closest.getGeometry().getName();
+                    System.out.println("Pointer picked " + hit + ".");
 
-                float dist = closest.getDistance();
-                if(dist < 10f){
-                    System.out.println("Can perform an action.");
-                }
-                else{
-                    System.out.println("Too far.");
+                    float dist = closest.getDistance();
+                    if (dist < 10f) {
+                        System.out.println("Can perform an action.");
+                    } else {
+                        System.out.println("Too far.");
+                    }
+                } else {
+                    System.out.println("Nothing.");
                 }
             }
-            else{
-                System.out.println("Nothing.");
-            }
-        }
-        if (binding.equals("Up") && optSprint) {
-            sprint = isPressed;
-        }
-        if (binding.equals("Sprint") && up) {
-            sprint = isPressed;
         }
     }
 
---
+++ src/maze/Player.java
@@ -0,0 +1,238 @@
+package maze;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.objects.PhysicsCharacter;
+import com.jme3.light.SpotLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.renderer.Camera;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+import com.jme3.shadow.SpotLightShadowFilter;
+import com.jme3.shadow.SpotLightShadowRenderer;
+
+/**
+ * This is the class that encapsulated all of the character's actions
+ * The character is represented by a PhysicsCharacter and the flyCam from SimpleApplication
+ * The character has a flash light as well, which is done in the initFlashlight method
+ */
+public class Player {
+    private Node rootNode;
+    private AssetManager assetManager;
+    private Camera cam;
+    private PhysicsSpace physicsSpace;
+    private ViewPort viewPort;
+
+    private PhysicsCharacter physicsCharacter;
+    private SpotLight flashlightInner;
+    private SpotLight flashlightMiddle;
+    private SpotLight flashlightOuter;
+
+    private Vector3f walkDirection = new Vector3f(0, 0, 0);
+    private boolean left, right, up, down, sprint, optSprint;
+    private float headBob = 0;
+    private float lightSway = 0;
+
+    public static final float flashlightInnerIntensity = 0.95f;
+    public static final float flashlightMiddleIntensity = 0.315f;
+    public static final float flashlightOuterIntensity = 0.30f;
+
+    private static final float PLAYER_SPEED = 5.0f;
+    private static final float PLAYER_SPRINT = 3.0f;
+    private static final int SHADOWMAP_SIZE = 1024;
+
+    /**
+     * the constructor takes in the flyCam from SimpleApplication
+     * we modify this value in the class, but never reassign it
+     */
+    public Player(Node rootNode, AssetManager assetManager, Camera cam, PhysicsSpace physicsSpace, ViewPort viewPort) {
+        this.rootNode = rootNode;
+        this.assetManager = assetManager;
+        this.cam = cam;
+        this.physicsSpace = physicsSpace;
+        this.viewPort = viewPort;
+
+        initPhysicsCharacter();
+        initFlashlight();
+    }
+
+    /**
+     * a helper method for the constructor that initializes the physics character
+     */
+    private void initPhysicsCharacter() {
+        // the player collision model is done through a sphere
+        physicsCharacter = new PhysicsCharacter(new SphereCollisionShape(3.0f), 0.1f);
+
+        // player does not jump
+        physicsCharacter.setJumpSpeed(0);
+        physicsCharacter.setFallSpeed(20);
+        physicsCharacter.setGravity(30);
+
+        physicsSpace.add(physicsCharacter);
+    }
+
+    /**
+     * a helper method for initializing the flashlight
+     */
+    private void initFlashlight() {
+        flashlightInner = new SpotLight();
+        flashlightInner.setSpotRange(32);
+        flashlightInner.setSpotInnerAngle(0f * FastMath.DEG_TO_RAD);
+        flashlightInner.setSpotOuterAngle(64f * FastMath.DEG_TO_RAD);
+        flashlightInner.setColor(ColorRGBA.White.mult(flashlightInnerIntensity));
+        rootNode.addLight(flashlightInner);
+
+        flashlightMiddle = new SpotLight();
+        flashlightMiddle.setSpotRange(38);
+        flashlightMiddle.setSpotInnerAngle(32f * FastMath.DEG_TO_RAD);
+        flashlightMiddle.setSpotOuterAngle(64f * FastMath.DEG_TO_RAD);
+        flashlightMiddle.setColor(ColorRGBA.Orange.mult(flashlightMiddleIntensity));
+        rootNode.addLight(flashlightMiddle);
+        flashlightOuter = new SpotLight();
+        flashlightOuter.setSpotRange(28);
+        flashlightOuter.setSpotInnerAngle(33f * FastMath.DEG_TO_RAD);
+        flashlightOuter.setSpotOuterAngle(64f * FastMath.DEG_TO_RAD);
+        flashlightOuter.setColor(ColorRGBA.Gray.mult(flashlightOuterIntensity));
+        rootNode.addLight(flashlightOuter);
+
+        initShadows(flashlightInner);
+        initShadows(flashlightMiddle);
+        initShadows(flashlightOuter);
+    }
+
+    /**
+     * a helper method to initialize the shadows for each light source
+     */
+    private void initShadows(SpotLight flashlight) {
+        SpotLightShadowRenderer slsr = new SpotLightShadowRenderer(assetManager, SHADOWMAP_SIZE);
+        slsr.setLight(flashlight);
+        viewPort.addProcessor(slsr);
+        SpotLightShadowFilter slsf = new SpotLightShadowFilter(assetManager, SHADOWMAP_SIZE);
+        slsf.setLight(flashlight);
+        slsf.setEnabled(true);
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        fpp.addFilter(slsf);
+        viewPort.addProcessor(fpp);
+    }
+
+    /**
+     * set the spawn point of the player
+     * we separate this from the Player constructor, since the Maze needs to be generated after the player
+     */
+    public void setSpawn(int x, int z) {
+        physicsCharacter.setPhysicsLocation(new Vector3f(x, 3, z));
+    }
+
+    /**
+     * the key handler for the player
+     */
+    public boolean onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Left") && !isPressed) {
+            left = false;
+        } else if (binding.equals("Left")) {
+            left = isPressed;
+        } else if (binding.equals("Right") && !isPressed) {
+            right = false;
+        } else if (binding.equals("Right")) {
+            right = isPressed;
+        } else if (binding.equals("Up") && !isPressed) {
+            up = false;
+            sprint = false;
+        } else if (binding.equals("Up")) {
+            up = isPressed;
+        } else if (binding.equals("Down") && !isPressed) {
+            down = false;
+        } else if (binding.equals("Down")) {
+            down = isPressed;
+        } else if (binding.equals("Sprint") && !isPressed) {
+            sprint = false;
+        } else if (binding.equals("Sprint")) {
+            sprint = isPressed;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * the update function for the player
+     * this is called in simpleUpdate in Maze
+     */
+    public void update(float tpf) {
+        Vector3f camDir = cam.getDirection();
+        Vector3f camLeft = cam.getLeft();
+        Vector3f camLoc = cam.getLocation();
+        float speed = PLAYER_SPEED;
+
+        Vector3f plLocation = physicsCharacter.getPhysicsLocation();
+        plLocation.setY(plLocation.getY() + 7f);
+
+        Vector3f lightDir = camDir;
+
+        walkDirection.set(0, 0, 0);
+
+        if (sprint) {
+            if (headBob < 360){
+                headBob += 10*tpf;
+                plLocation.setY(plLocation.getY() + 0.2f*FastMath.sin(headBob));
+                plLocation.setX(plLocation.getX() + 0.1f*FastMath.sin(headBob));
+            }
+            else {
+                headBob = 0;
+            }
+            if (lightSway < 360){
+                lightSway += 7*tpf;
+                lightDir.setY(lightDir.getY() + 0.025f*FastMath.sin(lightSway));
+                flashlightInner.setColor(ColorRGBA.White.mult(flashlightInnerIntensity + 0.2f*FastMath.sin(lightSway)));
+                flashlightMiddle.setColor(ColorRGBA.Orange.mult(flashlightMiddleIntensity + 0.1f*FastMath.sin(lightSway)));
+                flashlightOuter.setColor(ColorRGBA.Gray.mult(flashlightOuterIntensity + 0.2f*FastMath.sin(lightSway)));
+            }
+            else {
+                lightSway = 0;
+            }
+            speed = PLAYER_SPRINT;
+        }
+        else {
+            if(headBob != 0){
+                headBob = 0;
+            }
+            if(lightSway != 0){
+                lightSway = 0;
+            }
+            flashlightInner.setColor(ColorRGBA.White.mult(flashlightInnerIntensity));
+            flashlightMiddle.setColor(ColorRGBA.Orange.mult(flashlightMiddleIntensity));
+            flashlightOuter.setColor(ColorRGBA.Gray.mult(flashlightOuterIntensity));
+        }
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+
+        // the user cannot gain vertical distance
+        walkDirection.setY(0);
+
+        physicsCharacter.setWalkDirection(walkDirection.divide(speed));
+        flashlightInner.setPosition(camLoc);
+        flashlightMiddle.setPosition(camLoc);
+        flashlightOuter.setPosition(camLoc);
+
+        flashlightInner.setDirection(lightDir);
+        flashlightMiddle.setDirection(lightDir);
+        flashlightOuter.setDirection(lightDir);
+
+        cam.setLocation(plLocation);
+    }
+}
